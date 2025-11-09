46e5f88f91f26556d96c9646bd042ba78056362e
Lorenca <loridardha15@gmail.com>
2025-11-09 20:42:28 +0100

    last

--- main.py
+++ main.py
@@ -6,11 +6,7 @@ def main():
     ucs_cost, ucs_path = uniform_cost_search(start_state)
     
 
-    print("========== UCS PATH ==========")
-    for step in ucs_path:
-        print(step)
-    print("Total UCS Cost:", ucs_cost)
-
+    
     
 
 if __name__ == "__main__":
