98ac31603882fdc1830898cbc9a8d873ffc5e895
Lorenca <loridardha15@gmail.com>
2025-11-09 20:31:25 +0100

    feature commit

--- main.py
+++ main.py
@@ -4,17 +4,14 @@ from astar import a_star_search
 
 def main():
     ucs_cost, ucs_path = uniform_cost_search(start_state)
-    a_cost, a_path = a_star_search(start_state)
+    
 
     print("========== UCS PATH ==========")
     for step in ucs_path:
         print(step)
     print("Total UCS Cost:", ucs_cost)
 
-    print("\n========== A* PATH ==========")
-    for step in a_path:
-        print(step)
-    print("Total A* Cost:", a_cost)
+    
 
 if __name__ == "__main__":
     main()
